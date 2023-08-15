dcc682818d8a5913e14b37d8ee55886aae915918
Glitch (atsmitharchitecture) <none>
2023-08-15 13:56:05 +0000

    ⚽🐂 Updated with Glitch

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,29 +1,33 @@
 import * as React from "react";
-import ImageOverlap from "../components/ImageOverlap.jsx";
+import Avatar from "../components/Avatar.jsx";
 
 export default function About() {
   return (
     <React.Fragment>
       <div className="about-container">
         <h1>Computational Designer & Programmer</h1>
-        <h2>I design, fabricate, and program things.</h2>
-        <ImageOverlap />
-        <br/>
+        <h2>I love to design, build, and program things.</h2>
+        <Avatar />
         <div className="parts-container">
           <div className="parts-left">
             <h2>Part Designer</h2>
-            <h3> 
-            - Architect (MAA)
-            </h3><br/>
-            <h3> 
-            - Fabrication
-            </h3><br/>
-            <h3> 
-            - Rationalization
-            </h3><br/>
-            <h3> 
-            - Visualization
-            </h3><br/>
+            <ul>
+              <li>
+                <h3>Architecture</h3>
+              </li>
+              <li>
+                <h3>Digital Fab.</h3>
+              </li>
+              <li>
+                <h3>3D Modeling</h3>
+              </li>
+              <li>
+                <h3>Graphic Design</h3>
+              </li>
+              <li>
+                <h3>UI/UX Design</h3>
+              </li>
+            </ul>
           </div>
           <div className="parts-center"></div>
           <div className="parts-right">
@@ -31,18 +35,23 @@ export default function About() {
               {" "}
               {"{"}Part Programmer{"}"}
             </h2>
-            <h3> 
-            - Software Plugins
-            </h3><br/>
-            <h3> 
-            - Frontend Web
-            </h3><br/>
-            <h3> 
-            - Backend Web
-            </h3><br/>
-            <h3> 
-            - GIS
-            </h3><br/>
+            <ul>
+              <li>
+                <h3>Software Plugins</h3>
+              </li>
+              <li>
+                <h3>Maps & GIS</h3>
+              </li>
+              <li>
+                <h3>Web Development</h3>
+              </li>
+              <li>
+                <h3>Data Wrangling</h3>
+              </li>
+              <li>
+                <h3>Algorithms</h3>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
