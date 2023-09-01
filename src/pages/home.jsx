a1c867c5ff60a9277549fd641b598c585854d80d
Glitch (atsmitharchitecture) <none>
2023-09-01 16:45:37 +0000

    🫒🏭 Updated Home Page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,21 @@ import * as React from "react";
 import { Link } from "wouter";
 
 export default function Home() {
-
-return (
-      <div className="page">
-      <h2>This site is still under construction.</h2>
-    <h2>...but please have a look around.</h2>
+  return (
+    <div className="page">
+      <div className="home-header">
+        <h1>Hi, I'm Andrew.</h1>
+        <h2>I'm an architectural designer <br/> and part-time web developer.</h2>
+        <div className="home-intro">
+        <p>It's here you can </p>
+        <Link href="/projects" className="animatedLinkLeft">browse my work</Link>
+        <p>, <br/></p>  
+        <Link href="/about" className="animatedLinkLeft">get to know me</Link>
+        <p>, or </p>  
+        <Link href="/contact" className="animatedLinkLeft">get in touch</Link>
+        <p>.</p>    
+        </div>
+      </div>
     </div>
   );
 }
