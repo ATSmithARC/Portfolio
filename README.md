b52d0b2163f1b2e3f14437d379644b16d0238263
Glitch (atsmitharchitecture) <none>
2023-09-06 12:34:33 +0000

    🔠👺 Added Skills Section and Bio Links

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# My Portfolio in React
+# A Static Portfolio App Built w/ React
 
-This project contains source code for a simple web portfolio built using the React framework. I develop and host the development version of the site using the Glitch IDE.
-The production version of this site exists at [atsmitharc.com](https://www.atsmitharc.com)
+This project contains source code for my personal web portfolio.
 
-The media content for the webpage is hosted on Google S3 Cloud Storage.
+It'd developed with the React library and built with Vite.
+
+The build files are hosted from the gh-pages branch of this repo on a custom domain.
+
+You can visit the built site here: [atsmitharc.com](https://www.atsmitharc.com)
+
+The media content for the webpage is self-hosted on my own Google S3 Bucket.
+
+There are many things I'd like to improve, but for now it works :)
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { animated } from "react-spring";
 import { useWiggle } from "../hooks/wiggle";
-import ReactHtmlParser from 'react-html-parser';
+import ReactHtmlParser from "react-html-parser";
 import DropdownList from "../components/DropdownList.jsx";
 import a from "../data/about-data.json";
 import ExperienceSection from "../components/ExperienceSection.jsx";
@@ -10,15 +10,15 @@ export default function About() {
   const [style, trigger] = useWiggle({ x: 5, y: 5, scale: 1 });
   const api = "https://storage.googleapis.com/atsmitharc-media/thumb/degree";
   const ext = ".webp";
-  
+
   return (
     <React.Fragment>
       <div className="about-container">
-        <h1>Developer, Designer, Human</h1>
-        <h2>I love to design, build, and program things.</h2>
+        {/*<h1>Developer, Designer, Human</h1>
+        <h2>I love to design, build, and program things.</h2> */}
         <div className="three-columns-flex">
           <div id="right" className="about-column">
-            <DropdownList profile={a.human}/>
+            <DropdownList profile={a.human} />
           </div>
           <div id="middle" className="about-column">
             <DropdownList profile={a.designer} />
@@ -34,7 +34,7 @@ export default function About() {
           </div>
         </div>
         <h1 className="about-section-header">{a.profile.title}</h1>
-        <section className="three-columns-flex">
+        <section className="three-columns-flex about-me">
           <div className="about-column img-column">
             <img
               src={api + "-800" + ext}
@@ -51,10 +51,55 @@ export default function About() {
             <p>{ReactHtmlParser(a.profile.text[3])}</p>
           </div>
         </section>
-        <h1 className="about-section-header">My Experience:</h1>
-        <ExperienceSection key={0} experienceType={a.professional}/>
-        <h1 className="about-section-header">My Education:</h1>
-        <ExperienceSection key={1} experienceType={a.academic}/>
+        <h1 className="about-section-header">{a.professional.title}</h1>
+        <ExperienceSection key={0} experienceType={a.professional} />
+        <h1 className="about-section-header">{a.academic.title}</h1>
+        <ExperienceSection key={1} experienceType={a.academic} />
+        <h1 className="about-section-header">{a.skills.title}</h1>
+        <section className="three-columns-flex skills">
+          <div className="about-column dev-column">
+            <h4>Languages:</h4>
+            <ul>
+              {a.skills.languages.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <h4>Libraries:</h4>
+            <ul>
+              {a.skills.libraries.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <h4>Other:</h4>
+            <ul>
+              {a.skills.other.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="about-column arch-column">
+            <h4>Applications:</h4>
+            <ul>
+              {a.skills.applications.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="about-column spacer-column">
+          <h4>Architectural:</h4>
+            <ul>
+              {a.skills.architectural.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <h4>Development:</h4>
+            <ul>
+              {a.skills.development.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </section>
       </div>
     </React.Fragment>
   );
